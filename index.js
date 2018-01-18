45ef7052795e22fdb2bd4fa54c1177cbe50abb7c
Aparus <rustamapaev@gmail.com>
2018-01-18 17:41:09 +0300

    files saving on the server: photo and voice

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 import TelegramBot from "node-telegram-bot-api";
 import config from "./config";
-
-//console.log("config", config);
+var MongoClient = require("mongodb").MongoClient;
+var http = require("http");
 
 const TOKEN = config.token;
 const bot = new TelegramBot(TOKEN, { polling: true });
 
-/* 
-// bot sends back your message...
 bot.on("message", msg => {
-  const { chat: { id }, text } = msg;
-  console.log(msg);
-  bot.sendMessage(id, text);
-}); */
-
-bot.on("polling_error", error => {
-  console.log(error); // => 'EFATAL'
+  MongoClient.connect(config.mongoUrl, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("cityChallengeBot");
+    dbo.collection("messages").insertOne(msg, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
+  });
+  if (msg.photo)
+    msg.photo.forEach(element => {
+      bot.downloadFile(element.file_id, "files/photo");
+    });
+  if (msg.voice) bot.downloadFile(msg.voice.file_id, "files/voice");
 });
+
+/* 
+// file download example
+bot.downloadFile(
+  "AgADAgADfKgxG1s3AUtXkCcAAX5i_T93EpwOAATRd7iqqn_lvf5UAAIC",
+  "photo"
+); */
 
 bot.onText(/\/start/, (msg, [source, match]) => {
   const { chat: { id, first_name, last_name }, text } = msg;
@@ -24,6 +36,17 @@ bot.onText(/\/start/, (msg, [source, match]) => {
   bot.sendMessage(id, welcomeMessage);
 });
 
+bot.on("polling_error", error => {
+  console.log(error); // => 'EFATAL'
+});
+
+http
+  .createServer(function(req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("Hello World!");
+    res.end();
+  })
+  .listen(8080);
 /* 
 // message object example, that recievs server on event: bot.on("message", msg =>  
 
@@ -44,3 +67,11 @@ bot.onText(/\/start/, (msg, [source, match]) => {
   date: 1516191119,
   text: 'hahaha' }
 */
+
+/* 
+// bot sends back your message...
+bot.on("message", msg => {
+  const { chat: { id }, text } = msg;
+  console.log(msg);
+  bot.sendMessage(id, text);
+}); */
